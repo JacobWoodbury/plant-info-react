7934f82da2f992e0154eaa4468cf90fd13ada9aa
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-10-02 14:34:57 -0700

    Initial card for redwood

---
+++ src/components/Card2.jsx
@@ -0,0 +1,9 @@
+export default function Card2() {
+    return (
+        <>
+            <h2 id = "redwood">Redwood Trees</h2>
+            <p>Climate: 7a - 10a</p>
+            <a href = "https://en.wikipedia.org/wiki/Sequoioideae">Redwood Wiki</a>
+        </>
+    )
+}
