a77c4beaa221de25fbf26fb901bb43e687c1c684
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-10-02 14:31:24 -0700

    pine

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,8 @@
 export default function Card(){
    <>
+        <h2 id = "name">Pine Tree</h2>
+        <p id = "climate">3a-8a</p>
+        
+        <a href = "https://en.wikipedia.org/wiki/Pine">Pine Wiki</a>
    </>
 }
