7eefec184ceed7bc581096cef25c950f82cb741b
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-10-02 14:40:09 -0700

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Card from './components/Card'
+import Card2 from './components/Card2'
 import './App.css'
 
 function App() {
@@ -7,7 +9,9 @@ function App() {
   return (
     <>
       <Header/>
-
+        <Row></Row>
+        <Row></Row>
+        <Row></Row>
       <Footer/>
     </>
   )
---
+++ src/components/Row.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card"
+import Card2 from "./Card2"
+export default function Row(){
+    return(
+        <>
+            <Card></Card>
+            <div id = "card-spacer"></div>
+            <Card2></Card2>
+        
+        </>
+    )
+}
