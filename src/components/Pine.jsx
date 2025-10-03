0e9487aecb38805b143b814f658530e3498a975c
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-10-02 20:38:45 -0700

    Pine Tree Card test

--- src/components/Pine.jsx
+++ src/components/Pine.jsx
@@ -8,7 +8,29 @@ export default function Card(){
                         <td>Pine Tree Info:</td>
                         <td>
                             <ul>
-                                <li>Pines are widely distributed in the Northern Hemisphere</li>
+                                <li>Pines are widely distributed in the Northern Hemisphere. They occupy large areas of taiga, or boreal forest, but can be found in many habitats</li>
+                                <li>Pine trees provide one of the most extensively used types of timber. Not only that, but their seeds make pesto, and pine resin is flavor for retsina wine.</li>
+                                <li>Pine trees can grow up to 10-260 feet tall, the average height being 50-150 feet tall.</li>
+                                <li>Pine trees are very long lived, as they can typically reach ages of 100-1,000 years old, and some are even older! There have been reports of 4,800-4,900 year old pine trees!</li>
+                                <li>The bark of most pines is thick and scaly, but some may also have thin and flaky bark. As for the leaves, they may have seed leaves, juvenle leaves, scale leaves, and needles!</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Read more about Pine Trees:</td>
+                        <td>
+                            <ul>
+                                <li><a href="https://en.wikipedia.org/wiki/Pine" target = "_blank">Wikipedia</a></li>
+                                <li><a href = "https://www.amazingfactshome.com/fun-facts-about-pine-trees/" target = "_blank">Facts about Pine Trees</a></li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Videos on Pine Trees</td>
+                        <td>
+                            <ul>
+                                <li><a href = "https://www.youtube.com/watch?v=QvIVOxMBNB4" target = "_blank">Pine Tree Facts</a></li>
+                                <li><a href = "https://www.youtube.com/watch?v=7y4K2yMuOdQ" target = "_blank"></a>Pine Tree Identification</li>
                             </ul>
                         </td>
                     </tr>
